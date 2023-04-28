d0443b28c318ebdab7c4b51203e71f55f765b5e3
davezachry <davezachry@gmail.com>
2023-04-28 14:07:43 -0500

    Update these content files as well

--- application/pages/contact/index.php
+++ application/pages/contact/index.php
@@ -8,8 +8,7 @@
         ],
         'content' => [
             'title' => 'Contact Title',
-            'body' => '<p>Suspendisse potenti. Donec tempor odio vitae urna lobortis aliquet. Donec ut rhoncus risus, eget consequat tellus. Vivamus nec lacinia turpis. Morbi iaculis lectus quis tempus condimentum. Duis pretium pretium dui, vitae sodales nibh sodales nec. Vestibulum non nisi a elit pharetra accumsan. Fusce feugiat massa sit amet molestie suscipit. Nulla facilisi.</p>',
-            'module' => 'TEST'
+            'body' => '<p>Suspendisse potenti. Donec tempor odio vitae urna lobortis aliquet. Donec ut rhoncus risus, eget consequat tellus. Vivamus nec lacinia turpis. Morbi iaculis lectus quis tempus condimentum. Duis pretium pretium dui, vitae sodales nibh sodales nec. Vestibulum non nisi a elit pharetra accumsan. Fusce feugiat massa sit amet molestie suscipit. Nulla facilisi.</p>'
         ]
     ];
 ?>
